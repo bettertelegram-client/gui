9b0789c918373bf226e528cb38ede63c64783c83
BetterTelegram <lukas.kodstrom@protonmail.com>
2025-08-23 23:44:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,231 +1,182 @@
-const nightModeToggle = document.getElementById('nightModeToggle')
-const body = document.body
 
-const savedTheme = localStorage.getItem('theme')
-if (savedTheme === 'dark') {
-	body.classList.add('dark')
-	nightModeToggle.checked = true
-} else {
-	body.classList.add('light')
-}
-nightModeToggle.addEventListener('change', function () {
-	if (this.checked) {
-		body.classList.remove('light')
-		body.classList.add('dark')
-		localStorage.setItem('theme', 'dark')
-	} else {
-		body.classList.remove('dark')
-		body.classList.add('light')
-		localStorage.setItem('theme', 'light')
-	}
-})
+    const modal = document.getElementById('modal');
+    const modal_ok = document.getElementById('modal-ok');
+    if (modal_ok) modal_ok.addEventListener('click', () => modal.classList.remove('show'));
+    
+    function show_modal(message) {
+        document.getElementById('modal-message').innerHTML = message;
+        modal.classList.add('show');
+    }
 
-const modal = document.getElementById('modal')
-const modal_ok = document.getElementById('modal-ok')
-if (modal_ok)
-	modal_ok.addEventListener('click', () => modal.classList.remove('show'))
+    function format_uptime(uptime_secs) {
+        const months = Math.floor(uptime_secs / (30 * 24 * 3600));
+        uptime_secs %= (30 * 24 * 3600);
+        const weeks = Math.floor(uptime_secs / (7 * 24 * 3600));
+        uptime_secs %= (7 * 24 * 3600);
+        const days = Math.floor(uptime_secs / (24 * 3600));
+        uptime_secs %= (24 * 3600);
+        const hours = Math.floor(uptime_secs / 3600);
+        uptime_secs %= 3600;
+        const minutes = Math.floor(uptime_secs / 60);
+        
+        let formatted_uptime = '';
+        if (months > 0) formatted_uptime += `${months} month${months > 1 ? 's' : ''}, `;
+        if (weeks > 0) formatted_uptime += `${weeks} wk${weeks > 1 ? 's' : ''}, `;
+        if (days > 0) formatted_uptime += `${days} day${days > 1 ? 's' : ''}, `;
+        if (hours > 0) formatted_uptime += `${hours} hr${hours > 1 ? 's' : ''}, `;
+        if (minutes > 0) formatted_uptime += `${minutes} min${minutes > 1 ? 's' : ''}`;
+        formatted_uptime = formatted_uptime.replace(/, $/, '');
+    
+        const formatted_time = new Date((new Date()).getTime() - uptime_secs * 1000)
+            .toISOString().replace('T', ' ')
+            .slice(0, 19)
+            .split('-').reverse().join('-');
+        formatted_uptime += ` (${formatted_time.substring(formatted_time.indexOf(' ') + 1)})`;
+        return `Since ${formatted_uptime}`;
+    }
 
-function show_modal(message) {
-	document.getElementById('modal-message').innerHTML = message
-	modal.classList.add('show')
-}
-
-function format_uptime(uptime_secs) {
-	const months = Math.floor(uptime_secs / (30 * 24 * 3600))
-	uptime_secs %= 30 * 24 * 3600
-	const weeks = Math.floor(uptime_secs / (7 * 24 * 3600))
-	uptime_secs %= 7 * 24 * 3600
-	const days = Math.floor(uptime_secs / (24 * 3600))
-	uptime_secs %= 24 * 3600
-	const hours = Math.floor(uptime_secs / 3600)
-	uptime_secs %= 3600
-	const minutes = Math.floor(uptime_secs / 60)
-
-	let formatted_uptime = ''
-	if (months > 0)
-		formatted_uptime += `${months} month${months > 1 ? 's' : ''}, `
-	if (weeks > 0) formatted_uptime += `${weeks} wk${weeks > 1 ? 's' : ''}, `
-	if (days > 0) formatted_uptime += `${days} day${days > 1 ? 's' : ''}, `
-	if (hours > 0) formatted_uptime += `${hours} hr${hours > 1 ? 's' : ''}, `
-	if (minutes > 0) formatted_uptime += `${minutes} min${minutes > 1 ? 's' : ''}`
-	formatted_uptime = formatted_uptime.replace(/, $/, '')
-
-	const formatted_time = new Date(new Date().getTime() - uptime_secs * 1000)
-		.toISOString()
-		.replace('T', ' ')
-		.slice(0, 19)
-		.split('-')
-		.reverse()
-		.join('-')
-	formatted_uptime += ` (${formatted_time.substring(
-		formatted_time.indexOf(' ') + 1
-	)})`
-	return `Since ${formatted_uptime}`
-}
-
-let previous_href = window.location.href,
-	uptime_secs = parseInt(sessionStorage.getItem('srv_uptime')),
-	refresh_ticker = 0
-setInterval(() => {
-	const current_href =
-		window.location.href.indexOf('/') !== -1
-			? window.location.href.split('/').pop()
-			: window.location.href
-	if (current_href !== previous_href) {
-		previous_href = current_href
-		switch (previous_href) {
-			case 'Notes.html':
-				{
-					var dates = []
-					var versions = []
-					var notes = {}
-					let currentIndex = 0
-					const tabsContainer = document.getElementById('tabsContainer')
-					const notesList = document.getElementById('notesList')
-					const leftButton = document.getElementById('leftButton')
-					const rightButton = document.getElementById('rightButton')
-					function renderTabs() {
-						tabsContainer.innerHTML = versions
-							.map(
-								(version, index) => `
+    let previous_href = window.location.href, uptime_secs = parseInt(sessionStorage.getItem('srv_uptime')), refresh_ticker = 0;
+    setInterval(() => {
+        const current_href = window.location.href.indexOf('/') !== -1 ? window.location.href.split('/').pop() : window.location.href;
+        if (current_href !== previous_href) {
+            previous_href = current_href;
+            switch (previous_href) {
+                case 'Notes.html': {
+                    var dates = [];
+                    var versions = [];
+                    var notes = {};
+                    let currentIndex = 0;
+                    const tabsContainer = document.getElementById('tabsContainer');
+                    const notesList = document.getElementById('notesList');
+                    const leftButton = document.getElementById('leftButton');
+                    const rightButton = document.getElementById('rightButton');
+                    function renderTabs() {
+                        tabsContainer.innerHTML = versions
+                            .map(
+                                (version, index) => `
                                 <div class="tab ${
-																	index === currentIndex ? 'active' : ''
-																}" data-index="${index}">
+                                    index === currentIndex ? 'active' : ''
+                                }" data-index="${index}">
                                         <span>${version}</span>
                                         <p>${dates[index]}</p>
                                 </div>
                         `
-							)
-							.join('')
-					}
-
-					function renderNotes() {
-						const currentVersion = versions[currentIndex]
-						notesList.innerHTML = notes[currentVersion]
-							.map(note => `<li>${note}</li>`)
-							.join('')
-					}
-
-					function updateButtonState(button, isEnabled) {
-						const svgPath = button.querySelector('path')
-						button.classList.toggle('active', isEnabled)
-						svgPath.setAttribute('opacity', isEnabled ? '1' : '0.25')
-					}
-
-					function updateButtonVisibility() {
-						updateButtonState(leftButton, currentIndex > 0)
-						updateButtonState(rightButton, currentIndex < versions.length - 1)
-					}
-
-					tabsContainer.addEventListener('click', e => {
-						const tab = e.target.closest('.tab')
-						if (tab) {
-							currentIndex = parseInt(tab.dataset.index)
-							renderTabs()
-							renderNotes()
-							updateButtonVisibility()
-						}
-					})
-
-					leftButton.addEventListener('click', () => {
-						if (currentIndex > 0) {
-							--currentIndex
-							renderTabs()
-							renderNotes()
-						}
-						updateButtonVisibility()
-					})
-
-					rightButton.addEventListener('click', () => {
-						if (currentIndex < versions.length - 1) {
-							++currentIndex
-							renderTabs()
-							renderNotes()
-						}
-						updateButtonVisibility()
-					})
-
-					fetch('https://bettertelegram.org/release_notes', { method: 'GET' })
-						.then(response => response.json())
-						.then(json => {
-							if (json.err === 0) {
-								const data = JSON.parse(json.rel_info)
-								console.dir(data)
-								versions = data.versions
-								notes = data.notes
-								dates = data.dates
-								renderTabs()
-								renderNotes()
-								updateButtonVisibility()
-							}
-						})
-						.catch(error => {
-							console.error('error:', error)
-						})
-				}
-				break
-			case 'Setting.html':
-				{
-					const licence_key = document.getElementById('licence_key_text')
-					const licence_day = document.getElementById('licence_day_text')
-					if (licence_key && licence_day) {
-						licence_key.innerHTML = sessionStorage
-							.getItem('licence_key')
-							.replace(/(\d{4})(?=\d)/g, '$1 ')
-						licence_day.innerHTML = sessionStorage.getItem('licence_days')
-					}
-				}
-				break
-			case 'About.html':
-				{
-					const app_version = document.getElementById('app_version')
-					const dll_version = document.getElementById('dll_version')
-					if (app_version && dll_version) {
-						app_version.innerHTML = sessionStorage.getItem('app_version')
-						dll_version.innerHTML = sessionStorage.getItem('dll_version')
-					}
-				}
-				break
-			case 'Home.html':
-				{
-					if (sessionStorage.getItem('licence_days') <= 0) {
-						setTimeout(
-							() =>
-								show_modal(
-									'Please note that there are no days remaining on your licence. If you wish to continue using BetterTelegram, then head over to the settings page & add more days to your licence. </br> Thank you!'
-								),
-							3333
-						)
-					}
-				}
-				break
-			case 'Plugins.html':
-				{
-					const otr_plugin = document.querySelector('.plugin-toggle-otr')
-					if (otr_plugin) {
-						const should_enable = sessionStorage.getItem('plugin-otr') == 1
-						if (should_enable) otr_plugin.classList.add('active')
-						otr_plugin.checked = should_enable
-					}
-					const ghost_plugin = document.querySelector('.plugin-toggle-ghost')
-					if (ghost_plugin) {
-						const should_enable = sessionStorage.getItem('plugin-ghost') == 1
-						if (should_enable) ghost_plugin.classList.add('active')
-						ghost_plugin.checked = should_enable
-					}
-					const purge_plugin = document.querySelector('.plugin-toggle-purge')
-					if (purge_plugin) {
-						const should_enable = sessionStorage.getItem('plugin-purge') == 1
-						if (should_enable) purge_plugin.classList.add('active')
-						purge_plugin.checked = should_enable
-					}
-				}
-				break
-		}
-	}
-	if (refresh_ticker >= 10) {
-		const srv_uptime = document.getElementById('uptime_date')
-		if (srv_uptime) srv_uptime.innerHTML = format_uptime(uptime_secs)
-		refresh_ticker = 0
-	} else ++refresh_ticker
-}, 33)
+                            )
+                            .join('');
+                    }
+    
+                    function renderNotes() {
+                        const currentVersion = versions[currentIndex]
+                        notesList.innerHTML = notes[currentVersion]
+                            .map(note => `<li>${note}</li>`)
+                            .join('');
+                    }
+    
+                    function updateButtonState(button, isEnabled) {
+                        const svgPath = button.querySelector('path');
+                        button.classList.toggle('active', isEnabled);
+                        svgPath.setAttribute('opacity', isEnabled ? '1' : '0.25');
+                    }
+    
+                    function updateButtonVisibility() {
+                        updateButtonState(leftButton, currentIndex > 0);
+                        updateButtonState(rightButton, currentIndex < versions.length - 1);
+                    }
+    
+                    tabsContainer.addEventListener('click', e => {
+                        const tab = e.target.closest('.tab');
+                        if (tab) {
+                            currentIndex = parseInt(tab.dataset.index);
+                            renderTabs();
+                            renderNotes();
+                            updateButtonVisibility();
+                        }
+                    });
+    
+                    leftButton.addEventListener('click', () => {
+                        if (currentIndex > 0) {
+                            --currentIndex;
+                            renderTabs();
+                            renderNotes();
+                        }
+                        updateButtonVisibility();
+                    });
+    
+                    rightButton.addEventListener('click', () => {
+                        if (currentIndex < versions.length - 1) {
+                            ++currentIndex;
+                            renderTabs();
+                            renderNotes();
+                        }
+                        updateButtonVisibility();
+                    });
+    
+                    fetch('https://bettertelegram.org/release_notes', { method: 'GET' })
+                    .then(response => response.json())
+                    .then(json => {
+                        if (json.err === 0) {
+                            const data = JSON.parse(json.rel_info);
+                            console.dir(data);
+                            versions = data.versions;
+                            notes = data.notes;
+                            dates = data.dates;
+                            renderTabs();
+                            renderNotes();
+                            updateButtonVisibility();
+                        }
+                    })
+                    .catch(error => {
+                        console.error('error:', error);
+                    });
+                } break;
+                case 'Setting.html': {
+                    const licence_key = document.getElementById('licence_key_text');
+                    const licence_day = document.getElementById('licence_day_text');
+                    if (licence_key && licence_day) {
+                        licence_key.innerHTML = sessionStorage.getItem('licence_key').replace(/(\d{4})(?=\d)/g, '$1 ');
+                        licence_day.innerHTML = sessionStorage.getItem('licence_days');
+                    }
+                } break;
+                case 'About.html': {
+                    const app_version = document.getElementById('app_version');
+                    const dll_version = document.getElementById('dll_version');
+                    if (app_version && dll_version) {
+                        app_version.innerHTML = sessionStorage.getItem('app_version');
+                        dll_version.innerHTML = sessionStorage.getItem('dll_version');
+                    }
+                } break;
+                case 'Home.html': {
+                    if (sessionStorage.getItem('licence_days') <= 0) {
+                        setTimeout(() => 
+                            show_modal('Please note that there are no days remaining on your licence. If you wish to continue using BetterTelegram, then head over to the settings page & add more days to your licence. </br> Thank you!'), 3333);
+                    }
+                } break;
+                case 'Plugins.html': {
+                    const otr_plugin = document.querySelector('.plugin-toggle-otr');
+                    if (otr_plugin) {
+                        const should_enable = sessionStorage.getItem('plugin-otr') == 1;
+                        if (should_enable) otr_plugin.classList.add('active');
+                        otr_plugin.checked = should_enable;
+                    }
+                    const ghost_plugin = document.querySelector('.plugin-toggle-ghost');
+                    if (ghost_plugin) {
+                        const should_enable = sessionStorage.getItem('plugin-ghost') == 1;
+                        if (should_enable) ghost_plugin.classList.add('active');
+                        ghost_plugin.checked = should_enable;
+                    }
+                    const purge_plugin = document.querySelector('.plugin-toggle-purge');
+                    if (purge_plugin) {
+                        const should_enable = sessionStorage.getItem('plugin-purge') == 1;
+                        if (should_enable) purge_plugin.classList.add('active');
+                        purge_plugin.checked = should_enable;
+                    }
+                } break;
+            }
+        }
+        if (refresh_ticker >= 10) {
+            const srv_uptime = document.getElementById('uptime_date');
+            if (srv_uptime) srv_uptime.innerHTML = format_uptime(uptime_secs);
+            refresh_ticker = 0;
+        } else
+        ++refresh_ticker;
+    }, 33);
